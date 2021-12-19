d9b9160ca3c422ee623ac888749a4739f3408f23
Donna Dominguez Gamboa <donna.gamboa@whitecloak.com>
2021-12-18 19:55:42 -0800

    add widgets for other players

--- lib/widgets/game_other_player_status.dart
+++ lib/widgets/game_other_player_status.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mindmerge/constants/colours.dart';
 import 'package:mindmerge/models/player.dart';
 
 class OtherPlayerStatus extends StatefulWidget {
@@ -18,18 +20,56 @@ class _OtherPlayerStatusState extends State<OtherPlayerStatus> {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200,
-      child: Row(
-        children: [
-          if (widget.player.starCard != null)
+      width: 100,
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (widget.player.starCard != null)
+              Column(
+                children: [
+                  const Icon(
+                    Icons.star,
+                    color: secondaryColor,
+                  ),
+                  Text('${widget.player.starCard}'),
+                ],
+              ),
+            const SizedBox(width: 5),
             Column(
               children: [
-                Icon(
-                  Icons.star,
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: lightColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Opacity(
+                    opacity: 0.5,
+                    child: SvgPicture.asset(
+                      'assets/icons/shrimp.svg',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ),
-                Text('${widget.player.starCard}'),
+                SizedBox(
+                  width: 60,
+                  child: Text(
+                    widget.player.name.toUpperCase(),
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontFamily: 'BalooBhai',
+                      color: secondaryColor,
+                    ),
+                  ),
+                ),
               ],
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
